ab13745cb758e197b1a973a6153a9f7c26dea33d
hville <hugov@runbox.com>
2016-09-16 23:57:03 +0800

    API change to iterator

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var shuffle = require('array-shuffle')
 
+module.exports = latinSquare
+
 /**
  * Creates a random row generating function meeting the lating square restriction
  * usage:
@@ -9,19 +11,15 @@ var shuffle = require('array-shuffle')
  * @returns {Function} row generating function
  */
 function latinSquare (row) {
-	var sN = row.length
-	var rowCount = 0
+	var sN = row.length,
+			rowCount = 0
 
 	// prepare array of row and col indices for pre-sorting
-	var hSort = Array(sN)
-	for (var i = 0; i < sN; ++i)hSort[i] = i
-	var vSort = hSort.slice()
-
-	shuffle(hSort)
-	shuffle(vSort)	// random vertical shffle of rows
+	var hSort = shuffle(sequence(sN)),
+			vSort = shuffle(hSort)
 
 	function nextRow (tgt) {
-		if (rowCount === sN) return (tgt = null)	// no more unique rows available
+		if (rowCount === sN) return {done: true}
 		if (Array.isArray(tgt)) tgt.length = sN
 		else tgt = Array(sN)
 
@@ -32,10 +30,12 @@ function latinSquare (row) {
 		}
 		rowCount++
 
-		return tgt
+		return {value: tgt}
 	}
 
-	return nextRow
+	return {next: nextRow}
 }
-
-module.exports = latinSquare
+function sequence(n) {
+	for (var i = 0, a=Array(n); i < n; ++i) a[i] = i
+	return a
+}
--- test.js
+++ test.js
@@ -3,51 +3,59 @@ var c = require('cotest')
 var latinSquare = require('./index')
 
 var N = 8,
-		samples = [],
-		i, j
-
-for (i = 0; i < N; ++i) samples[i] = i
+		samples = sequence(N)
 
 var sampler = latinSquare(samples)
 
+c('get a new row', function() {
+	sampler = latinSquare(samples)
+	c('===', sampler.next().value.length, N)
+})
 c('add samples to an existing array', function() {
 	var arr = []
 	sampler = latinSquare(samples)
-	sampler(arr)
+	sampler.next(arr)
 	c('===', arr.length, N)
 })
-c('unique row combinations', function() {
-	var mat = [],
-			pass = true
+c('unique row and col combinations', function() {
+	var mat = []
+
+	//avoid false positives
+	c('==', uniqueInRow([[1,2],[2,1]]), true)
+	c('==', uniqueInRow([[1,1],[2,1]]), false)
+	c('==', uniqueInCol([[2,1],[1,2]]), true)
+	c('==', uniqueInCol([[2,1],[2,2]]), false)
+
 	sampler = latinSquare(samples)
-	for (i = 0; i < N; ++i) { mat[i] = sampler() }
-	for (i = 0; i < N; ++i) {
-		for (j = 0; j < N; j+++j) {
-			if (mat[i].indexOf(mat[i][j]) !== j) pass = false
+	for (var i = 0; i < N; ++i) { mat[i] = sampler.next().value }
+	c('==', uniqueInRow(mat), true)
+	c('==', uniqueInRow(mat), true)
+})
+c('iterator ends when samples are exhausted', function() {
+	sampler = latinSquare(samples)
+	for (var i = 0; i < N; ++i) sampler.next()
+	c('===', sampler.next().done, true)
+})
+function sequence(n) {
+	for (var i=0, a=[]; i<n; ++i) a[i] = i+1
+	return a
+}
+//Convention[row][col]
+function uniqueInRow(mat) {
+	for (var row = 0; row < mat.length; ++row) {
+		for (var col = 0; col < mat.length; ++col) {
+			var val = mat[row][col]
+			for (var i=row+1; i<mat.length; ++i) if (mat[i][col] === val) return false
 		}
 	}
-	c('==', pass, true)
-})
-c('unique col combinations', function() {
-	var mat = Array(N),
-			pass = true
-	sampler = latinSquare(samples)
-	for (i = 0; i < N; ++i) {
-		var smpl = sampler()
-		for (j = 0; j < N; j+++j) {
-			if (!Array.isArray(mat[j])) mat[j] = Array(N)
-			mat[j][i] = smpl[j]
+	return true
+}
+function uniqueInCol(mat) {
+	for (var row = 0; row < mat.length; ++row) {
+		for (var col = 0; col < mat.length; ++col) {
+			var val = mat[row][col]
+			for (var j=col+1; j<mat.length; ++j) if (mat[row][j] === val) return false
 		}
 	}
-	for (i = 0; i < N; ++i) {
-		for (var k = 0; k < N; k+++j) {
-			if (mat[i].indexOf(mat[i][k]) !== k) pass = false
-		}
-	}
-	c('==', pass, true)
-})
-c('returns null when samples are exhausted', function() {
-	sampler = latinSquare(samples)
-	for (i = 0; i < N; ++i) sampler()
-	c('===', sampler(), null)
-})
+	return true
+}
